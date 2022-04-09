b58060f708653fd0f8c653938a9299252ff17436
LORD-Zenix <nandananda2235@gmail.com>
2022-04-09 11:48:53 +0700

    Chika-Md

---
+++ settings.js
@@ -0,0 +1,59 @@
+/*
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6285701863281','6281386725865','6285733949045']
+global.premium = ['6285701863281']
+global.ownername = 'Zenix'
+global.botname = 'ZennBot-Md'
+global.footer = 'Zenix'
+global.ig = 'https://instagram.com/tn_zenix'
+global.email = 'nandananda2235@gmail.com'
+global.region = 'Indonesia'
+global.sc = 'https://github.com/LORD-Zenix/Chika-MD'
+global.myweb = 'https://api-riychdwayne.herokuap.com'
+global.packname = 'Hisoka Morrow'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'chika'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./image/chika.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
